dcaaa40a0528b7b9148742b5c7e6c1c8a729ef92
rhendges <66683429+rhendges@users.noreply.github.com>
2020-06-09 18:16:10 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,56 +1,58 @@
 #!/bin/bash
-
 #Script de personalização inicial da minha distro Debian
-#Executar como sudo
+#Trabalho em andamento... noob noob noob
 
-#Seleciona o mirror mais rápido para o apt
-sudo apt install -y netselect-apt
-sudo netselect-apt -c brazil -t 6 -a amd64 -n stable
-mv sources.list /etc/apt/sources.list
-
-#atualiza o apt e instala pacotes necessários
-apt update -y
-apt install -y wget git gcc python3 screenfetch software-properties-common
+#Instala dependências e ferramentas
+sudo apt update -y
+sudo apt install -y wget git gcc python3 screenfetch software-properties-common
+sudo apt install -y accountsservice gir1.2-atk-1.0 gir1.2-freedesktop gir1.2-gdkpixbuf-2.0 gir1.2-gst-plugins-base-1.0 gir1.2-gstreamer-1.0 gir1.2-gtk-3.0 gir1.2-pango-1.0 gstreamer1.0-gl libaccountsservice0 libgraphene-1.0-0 libgstreamer-gl1.0-0 libpangoxft-1.0-0 python3-cairo python3-pexpect python3-ptyprocess
 
 ###Criar if para escolha se já possui interface grafica instalada
 #apt install -y xorg xfce4 xfce4-goodies lightdm
 
-#instala pacote mugshot faltante
+#Instala pacote mugshot faltante
 cd /tmp
 wget http://ftp.br.debian.org/debian/pool/main/m/mugshot/mugshot_0.4.2-1_all.deb
-dpkg -i mugshot_0.4.2-1_all.deb
+sudo dpkg -i mugshot_0.4.2-1_all.deb
 
 #Baixa e instala temas
-mkdir -p ~/.themes
+mkdir -p /home/$USER/.themes
 wget https://github.com/rhendges/linux-scripts/raw/master/flat-remix-blue.xz
-tar -xvf flat-remix-blue.xz
-cp -r Flat-Remix-Blue/ ~/.icons/
-cp -r Flat-Remix-Blue/ /usr/share/icons/
+tar -xf flat-remix-blue.xz
+cp -r Flat-Remix-Blue/ /home/$USER/.icons/
+sudo cp -r Flat-Remix-Blue/ /usr/share/icons/
 rm -rf Flat-Remix-Blue/
 rm flat-remix-blue.xz
+
 wget https://github.com/rhendges/linux-scripts/raw/master/NumixHolo.zip
-unzip NumixHolo.zip
-cp -r NumixHolo/ ~/.themes/
-cp -r NumixHolo/ /usr/share/themes/
+unzip -qq -o NumixHolo.zip
+cp -r NumixHolo/ /home/$USER/.themes/
+sudo cp -r NumixHolo/ /usr/share/themes/
 rm -rf NumixHolo/
 rm NumixHolo.zip
-echo "screenfetch" >> ~/.bashrc
-echo "screenfetch" >> /etc/skel/.bashrc
+
 wget https://github.com/rhendges/linux-scripts/raw/master/xfce4.zip
-unzip xfce4.zip
+unzip -qq -o xfce4.zip
 rm xfce4.zip
+
 wget https://github.com/rhendges/linux-scripts/raw/master/.gtkrc-2.0
-cp .gtkrc-2.0 ~/
-cp .gtkrc-2.0 /etc/skel
+cp .gtkrc-2.0 /home/$USER/
+sudo cp .gtkrc-2.0 /etc/skel/
 
 #Define os temas
-cp -r panel/ ~/.config/xfce4
-cp -r terminal/ ~/.config/xfce4
-cp -r xfconf/ ~/.config/xfce4
+mkdir /home/$USER/.config
+cp -r panel/ /home/$USER/.config/xfce4/
+cp -r terminal/ /home/$USER/.config/xfce4/
+cp -r xfconf/ /home/$USER/.config/xfce4/
 
 #copia templates do usuário para /etc/skel
-mkdir /etc/skel/.config
-mkdir /etc/skel/.config/xfce4
-cp -r panel/ /etc/skel/.config/xfce4
-cp -r terminal/ /etc/skel/.config/xfce4
-cp -r xfconf/ /etc/skel/.config/xfce4
+sudo mkdir /etc/skel/.config
+sudo mkdir /etc/skel/.config/xfce4
+sudo cp -r panel/ /etc/skel/.config/xfce4/
+sudo cp -r terminal/ /etc/skel/.config/xfce4/
+sudo cp -r xfconf/ /etc/skel/.config/xfce4/
+
+
+#echo "screenfetch" >> /home/$USER/.bashrc
+#sudo echo "screenfetch" >> /etc/skel/.bashrc
+
