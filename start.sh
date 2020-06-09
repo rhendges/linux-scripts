a9a9fa0b74f8c5b4a24ed060d17d9d0ddb4c9582
rhendges <66683429+rhendges@users.noreply.github.com>
2020-06-09 15:22:15 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+
 #Script de personalização inicial da minha distro Debian
 #Executar como sudo
+
 apt update -y
 apt install -y wget git gcc python3 screenfetch software-properties-common
 
@@ -12,5 +14,27 @@ cd /tmp
 wget https://github.com/rhendges/linux-scripts/raw/master/flat-remix-blue.xz
 tar -xvf flat-remix-blue.xz
 cp -r Flat-Remix-Blue/ ~/.icons/
+cp -r Flat-Remix-Blue/ /usr/share/icons/
 rm -rf Flat-Remix-Blue/
+rm flat-remix-blue.xz
+wget https://github.com/rhendges/linux-scripts/raw/master/NumixHolo.zip
+unzip NumixHolo.zip
+cp -r NumixHolo/ ~/.themes/
+cp -r NumixHolo/ /usr/share/themes/
+rm -rf NumixHolo/
+rm NumixHolo.zip
+echo "screenfetch" >> ~/.bashrc
+echo "screenfetch" >> /etc/skel/.bashrc
+wget https://github.com/rhendges/linux-scripts/raw/master/xfce4.zip
+unzip xfce4.zip
+rm xfce4.zip
+cp -r panel/ ~/.config/xfce4
+cp -r terminal/ ~/.config/xfce4
+cp -r xfconf/ ~/.config/xfce4
 
+#copia templates do usuário para /etc/skel
+mkdir /etc/skel/.config
+mkdir /etc/skel/.config/xfce4
+cp -r panel/ /etc/skel/.config/xfce4
+cp -r terminal/ /etc/skel/.config/xfce4
+cp -r xfconf/ /etc/skel/.config/xfce4
