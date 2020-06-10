fd93dc7219a14f857ee6075f3fda8dbffec0d672
rhendges <66683429+rhendges@users.noreply.github.com>
2020-06-10 18:08:28 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@
 
 #Instala dependências e ferramentas
 sudo apt update -y
-sudo apt install -y wget git gcc python3 screenfetch software-properties-common
+sudo apt install -y wget git gcc vim ruby golang python3 screenfetch software-properties-common qterminal firefox-esr
 sudo apt install -y accountsservice gir1.2-atk-1.0 gir1.2-freedesktop gir1.2-gdkpixbuf-2.0 gir1.2-gst-plugins-base-1.0 gir1.2-gstreamer-1.0 gir1.2-gtk-3.0 gir1.2-pango-1.0 gstreamer1.0-gl libaccountsservice0 libgraphene-1.0-0 libgstreamer-gl1.0-0 libpangoxft-1.0-0 python3-cairo python3-pexpect python3-ptyprocess
 
 ###Criar if para escolha se já possui interface grafica instalada
@@ -16,17 +16,17 @@ wget http://ftp.br.debian.org/debian/pool/main/m/mugshot/mugshot_0.4.2-1_all.deb
 sudo dpkg -i mugshot_0.4.2-1_all.deb
 
 #Baixa e instala temas
-mkdir -p /home/$USER/.themes
+#mkdir -p /home/$USER/.themes
 wget https://github.com/rhendges/linux-scripts/raw/master/flat-remix-blue.xz
 tar -xf flat-remix-blue.xz
-cp -r Flat-Remix-Blue/ /home/$USER/.icons/
+#cp -r Flat-Remix-Blue/ /home/$USER/.icons/
 sudo cp -r Flat-Remix-Blue/ /usr/share/icons/
 rm -rf Flat-Remix-Blue/
 rm flat-remix-blue.xz
 
 wget https://github.com/rhendges/linux-scripts/raw/master/NumixHolo.zip
 unzip -qq -o NumixHolo.zip
-cp -r NumixHolo/ /home/$USER/.themes/
+#cp -r NumixHolo/ /home/$USER/.themes/
 sudo cp -r NumixHolo/ /usr/share/themes/
 rm -rf NumixHolo/
 rm NumixHolo.zip
