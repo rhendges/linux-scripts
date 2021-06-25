89e65f424832fb0caa7bcba737541e407a06bce4
rhendges <66683429+rhendges@users.noreply.github.com>
2021-06-25 10:18:22 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,12 +4,12 @@
 
 #Instala netselect para otimizar mirrors apt
 sudo apt install netselect-apt
-sudo netselect-apt -c Brazil -t 6 -a amd64 -n stable
+sudo netselect-apt -t 6 -a amd64 -n stable
 sudo mv sources.list /etc/apt/sources.list
 
 #Instala dependências e ferramentas
 sudo apt update -y
-sudo apt install -y wget git gcc vim ruby golang python3 screenfetch software-properties-common qterminal firefox-esr
+sudo apt install -y wget git gcc vim ruby golang python3 screenfetch software-properties-common qterminal firefox-esr xrdp
 sudo apt install -y accountsservice gir1.2-atk-1.0 gir1.2-freedesktop gir1.2-gdkpixbuf-2.0 gir1.2-gst-plugins-base-1.0 gir1.2-gstreamer-1.0 gir1.2-gtk-3.0 gir1.2-pango-1.0 gstreamer1.0-gl libaccountsservice0 libgraphene-1.0-0 libgstreamer-gl1.0-0 libpangoxft-1.0-0 python3-cairo python3-pexpect python3-ptyprocess
 
 ###Criar if para escolha se já possui interface grafica instalada
